5fc14cb262d8a44a66cb3f0ee2a0ff6c57cb1d71
kombolewis <kombolewis@gmail.com>
2022-06-14 13:26:45 +0300

    added tracked migrations

---
+++ migrations/Migration1655198940.php
@@ -0,0 +1,13 @@
+<?php
+  namespace Migrations;
+  use Core\Migration;
+
+  class Migration1655198940 extends Migration {
+    public function up() {
+      $table = "migrations";
+      $this->createTable($table);
+      $this->addColumn($table, 'migration', 'varchar',['size'=>35]);
+      $this->addIndex($table,'migration');
+    }
+  }
+  
---
+++ migrations/Migration1655199167.php
@@ -0,0 +1,26 @@
+<?php
+  namespace Migrations;
+  use Core\Migration;
+
+  class Migration1655199167 extends Migration {
+    public function up() {
+      $table = "product";
+      $this->createTable($table);
+      $this->addColumn($table,'sku','varchar',['size'=>200]);
+      $this->addColumn($table,'name','varchar',['size'=>200]);
+      $this->addColumn($table,'price','decimal',['size' => '10,2']);
+      $this->addColumn($table,'productType','varchar',['size'=>50]);
+  
+      $table = "product_description";
+      $this->createTable($table);
+      $this->addColumn($table,'attribute','varchar',['size'=>255]);
+      $this->addColumn($table,'value','varchar',['size'=>255]);
+      $this->addColumn($table,'product_id','int');
+
+      $constraint = "ALTER TABLE `product_description` ADD CONSTRAINT 
+        `product_description_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+      ";
+      $this->query($constraint);
+    }
+  }
+  
