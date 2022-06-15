e31fed28d27347094decd1c64dcae9f1f49d5c15
kombolewis <kombolewis@gmail.com>
2022-06-15 11:28:18 +0300

    modified config

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  // baseURL: 'https://ecomstore14.herokuapp.com'
-  baseURL: 'http://localhost:8000'
+  baseURL: 'https://ecomstore14.herokuapp.com'
+  // baseURL: 'http://localhost:8000'
 }
 
 export default config
