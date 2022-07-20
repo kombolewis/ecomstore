6545d6693cbc7a4938b4f2ba559ef967e78d4d45
kombolewis <kombolewis@gmail.com>
2022-07-21 02:51:43 +0300

    including environment configuration files

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
-define('DEBUG', false);
+define('DEBUG', $_ENV['DEBUG']);
 
-define('DB_NAME', ''); //database name
-define('DB_USER', ''); //database user
-define('DB_PASSWORD', ''); //database password
-define('DB_HOST', ''); //database host
+define('DB_NAME', $_ENV['DB_NAME']); //database name
+define('DB_USER', $_ENV['DB_USER']); //database user
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']); //database password
+define('DB_HOST', $_ENV['DB_HOST']); //database host
 
 define('DEFAULT_LAYOUT', 'default'); // if no layout is defined in layout
 
@@ -17,10 +17,4 @@ define('REMEMBER_ME_COOKIE_NAME', 'eolwiejfqmvrfergkdfgickds'); //cookie name fo
 define('REMEMBER_ME_COOKIE_EXPIRY', 2592000); 
 
 define('ACCESS_RESTRICTED', 'Restricted'); //controller name for restricted redirect
-
-
-//headers
-
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-Requested-With");
 
--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__file__));
 
-//load configuration
-
-require_once(ROOT . DS . 'config' . DS . 'config.php');
-
-
 //Autoload classes 
 require_once(ROOT . DS . 'core' . DS .'autoload.php');
 
+//load configuration
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+//load application configuration
+require_once(ROOT . DS . 'config' . DS . 'config.php');
 
 session_start();
 
@@ -20,15 +21,10 @@ use App\Controllers\AjaxController;
 
 $app = new Core\Application();
 
-
 $app->router->get('/', [ProductListController::class, 'index']);
 $app->router->get('/add-product', [AddProductController::class, 'create']);
 $app->router->post('/add-product', [AddProductController::class, 'store']);
 $app->router->post('/delete-items', [ProductListController::class, 'delete']);
 $app->router->post('/validate-unique', [AjaxController::class, 'index']);
-
-
-
-
 
 $app->run();
